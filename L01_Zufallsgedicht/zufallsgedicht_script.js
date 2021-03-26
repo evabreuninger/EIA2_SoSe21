88d155f654f2b695fac4de1394ddc136eb00db27
Eva Breuninger <eva.breuninger@hs-furtwangen.de>
2021-03-26 17:40:35 +0100

    fertigstellen L01

--- L01_Zufallsgedicht/zufallsgedicht_script.js
+++ L01_Zufallsgedicht/zufallsgedicht_script.js
@@ -1,3 +1,25 @@
 "use strict";
-console.log("Expecto Patronum");
+var Zufallsgedicht;
+(function (Zufallsgedicht) {
+    //Arrays
+    let subjekt = ["Eren", "Mikasa", "Armin", "Levi", "Erwin", "Hange"];
+    let prädikat = ["kämpft gegen", "liebt", "studiert", "hasst", "feiert", "zerstört"];
+    let objekte = ["Mauer Rose", "einen Titan", "Rekruten", "Marley", "den 3D-Manöver-Apparat", "den Aufklärungstrupp"];
+    console.log(objekte);
+    for (let index = subjekt.length; index > 0; index--) {
+        let verse = getVerse();
+        console.log(verse);
+        document.write("<p>" + verse + "</p>");
+    }
+    function getVerse() {
+        let value1 = Math.floor(Math.random() * subjekt.length);
+        let value2 = Math.floor(Math.random() * prädikat.length);
+        let value3 = Math.floor(Math.random() * objekte.length);
+        let value = subjekt[value1] + " " + prädikat[value3] + " " + objekte[value2];
+        subjekt.splice(value1, 1);
+        objekte.splice(value2, 1);
+        prädikat.splice(value3, 1);
+        return value;
+    }
+})(Zufallsgedicht || (Zufallsgedicht = {}));
 //# sourceMappingURL=zufallsgedicht_script.js.map
--- L01_Zufallsgedicht/zufallsgedicht_script.ts
+++ L01_Zufallsgedicht/zufallsgedicht_script.ts
@@ -1,1 +1,26 @@
-console.log("Expecto Patronum");
+namespace Zufallsgedicht {
+    //Arrays
+    let subjekt: string[] = ["Eren", "Mikasa", "Armin", "Levi", "Erwin", "Hange"];
+    let prädikat: string[] = ["kämpft gegen", "liebt", "studiert", "hasst", "feiert", "zerstört"];
+    let objekte: string[] = ["Mauer Rose", "einen Titan", "Rekruten", "Marley", "den 3D-Manöver-Apparat", "den Aufklärungstrupp"]
+    console.log(objekte);
+
+    for (let index = subjekt.length; index > 0 ; index--) {  
+        let verse: string = getVerse();
+        console.log(verse);
+        document.write("<p>" + verse + "</p>");
+    }
+
+    function getVerse(): string {
+        let value1: number = Math.floor(Math.random() * subjekt.length);
+        let value2: number = Math.floor(Math.random() * prädikat.length);
+        let value3: number = Math.floor(Math.random() * objekte.length);
+
+        let value: string = subjekt[value1] + " " + prädikat[value3] + " " + objekte[value2];
+
+        subjekt.splice(value1, 1);
+        objekte.splice(value2, 1);
+        prädikat.splice(value3, 1);
+        return value;
+    }
+}
