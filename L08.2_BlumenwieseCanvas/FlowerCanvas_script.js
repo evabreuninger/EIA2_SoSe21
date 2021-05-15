ad98cfdecbcd961817066d376e8eea6f2a6eb3a7
Eva Breuninger <eva.breuninger@hs-furtwangen.de>
2021-05-15 17:57:30 +0200

    Update Aufgabe L08.2
    Probleme mit meiner x-achse und der darstellung von Bumen

--- L08.2_BlumenwieseCanvas/FlowerCanvas_script.js
+++ L08.2_BlumenwieseCanvas/FlowerCanvas_script.js
@@ -1,2 +1,134 @@
 "use strict";
+// Inspiriert bei Jirka
+var L08_2_FlowerCanvas;
+(function (L08_2_FlowerCanvas) {
+    window.addEventListener("load", handleLoad);
+    let crc2;
+    let golden = 0.62;
+    function handleLoad(_event) {
+        let canvas = document.querySelector("canvas");
+        if (!canvas)
+            return;
+        crc2 = canvas.getContext("2d");
+        drawBackground();
+        drawSun({ x: 100, y: 75 });
+        drawCloud({ x: 500, y: 125 }, { x: 200, y: 60 });
+        drawMountains({ x: -500, y: 400 }, 75, 200, "grey", "white");
+        drawMountains({ x: -500, y: 400 }, 50, 150, "grey", "lightgrey");
+        drawBush({ x: -300, y: 500 }, { x: 50, y: 40 });
+        drawBush({ x: 0, y: 550 }, { x: 50, y: 40 });
+        drawTulip(0, 400);
+    }
+    function drawBackground() {
+        console.log("Background");
+        let gradient = crc2.createLinearGradient(0, 0, 0, crc2.canvas.height);
+        gradient.addColorStop(0, "lightblue");
+        gradient.addColorStop(golden, "white");
+        gradient.addColorStop(1, "green");
+        crc2.fillStyle = gradient;
+        crc2.fillRect(0, 0, crc2.canvas.width, crc2.canvas.height);
+    }
+    function drawSun(_position) {
+        console.log("Sun");
+        let r1 = 30;
+        let r2 = 150;
+        let gradient = crc2.createRadialGradient(0, 0, r1, 0, 0, r2);
+        gradient.addColorStop(0, "HSLA(60, 100%, 90%, 1)");
+        gradient.addColorStop(1, "HSLA(60, 100%, 50%, 0)");
+        crc2.save();
+        crc2.translate(_position.x, _position.y);
+        crc2.fillStyle = gradient;
+        crc2.arc(0, 0, r2, 0, 2 * Math.PI);
+        crc2.fill();
+        crc2.restore();
+    }
+    function drawCloud(_position, _size) {
+        console.log("Cloud", _position, _size);
+        let nParticles = 60;
+        let radiusParticle = 20;
+        let particle = new Path2D();
+        let gradient = crc2.createRadialGradient(0, 0, 0, 0, 0, radiusParticle);
+        particle.arc(0, 0, radiusParticle, 0, 2 * Math.PI);
+        gradient.addColorStop(0, "HSLA(0, 100%, 100%, 0.5)");
+        gradient.addColorStop(1, "HSLA(0, 100%, 100%, 0)");
+        crc2.save();
+        crc2.translate(_position.x, _position.y);
+        crc2.fillStyle = gradient;
+        for (let drawn = 0; drawn < nParticles; drawn++) {
+            crc2.save();
+            let x = (Math.random() - 0.5) * _size.x;
+            let y = -(Math.random() * _size.y);
+            crc2.translate(x, y);
+            crc2.fill(particle);
+            crc2.restore();
+        }
+    }
+    function drawMountains(_position, _min, _max, _colorLow, _colorHigh) {
+        console.log("Mountains");
+        let stepMin = 50;
+        let stepMax = 150;
+        let x = 0;
+        crc2.save();
+        crc2.translate(_position.x, _position.y);
+        crc2.beginPath();
+        crc2.moveTo(0, 0);
+        crc2.lineTo(0, -_max);
+        do {
+            x += stepMin + Math.random() * (stepMax - stepMin);
+            let y = -_min - Math.random() * (_max - _min);
+            crc2.lineTo(x, y);
+        } while (x < crc2.canvas.width);
+        crc2.lineTo(x, 0);
+        crc2.closePath();
+        let gradient = crc2.createLinearGradient(0, 0, 0, -_max);
+        gradient.addColorStop(0, _colorLow);
+        gradient.addColorStop(0.7, _colorHigh);
+        crc2.fillStyle = gradient;
+        crc2.fill();
+        crc2.restore();
+    }
+    function drawBush(_position, _size) {
+        console.log("Bush", _position, _size);
+        let nParticles = 10;
+        let radiusParticle = 20;
+        let particle = new Path2D();
+        let gradient = crc2.createRadialGradient(0, 0, 0, 0, 0, radiusParticle);
+        particle.arc(0, 0, radiusParticle, 0, 2 * Math.PI);
+        gradient.addColorStop(0, "green");
+        gradient.addColorStop(1, "darkgreen");
+        crc2.save();
+        crc2.translate(_position.x, _position.y);
+        crc2.fillStyle = gradient;
+        for (let drawn = 0; drawn < nParticles; drawn++) {
+            crc2.save();
+            let x = (Math.random() - 0.5) * _size.x;
+            let y = -(Math.random() * _size.y);
+            crc2.translate(x, y);
+            crc2.fill(particle);
+            crc2.restore();
+        }
+    }
+    function drawTulip(_x, _y) {
+        //Stiel
+        crc2.beginPath();
+        crc2.fillStyle = "HSLA(60, 100%, 50%, 0)";
+        crc2.fillRect(_x + 9, _y + 28, 3, 40);
+        //Blatt
+        crc2.arc(_x + 8, _y + 30, 20, 0, 1.5);
+        crc2.fill();
+        //Blüte
+        crc2.beginPath();
+        crc2.fillStyle = "#FF1493";
+        crc2.arc(_x + 10, _y + 20, 10, 0, 1 * Math.PI);
+        crc2.moveTo(_x, _y + 22);
+        crc2.lineTo(_x, _y + 7);
+        crc2.lineTo(_x + 6, _y + 14);
+        crc2.lineTo(_x + 10.5, _y + 4);
+        crc2.lineTo(_x + 15, _y + 14);
+        crc2.lineTo(_x + 20, _y + 7);
+        crc2.lineTo(_x + 20, _y + 21);
+        crc2.closePath();
+        crc2.fill();
+    }
+})(L08_2_FlowerCanvas || (L08_2_FlowerCanvas = {}));
 //# sourceMappingURL=FlowerCanvas_script.js.map
