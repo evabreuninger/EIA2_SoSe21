b34a4d68dc60bd1bf64248f743804c059c5ae777
Eva Breuninger <eva.breuninger@hs-furtwangen.de>
2021-06-14 19:07:29 +0200

    Ergänzung Vermerk

--- L10.2_BlumenwiesePolymorphie/script.ts
+++ L10.2_BlumenwiesePolymorphie/script.ts
@@ -1,4 +1,13 @@
 namespace eia10_2 {
+
+    /*
+    Aufgabe: L10.2_BlumenwiesePolymorphie
+    Name: Eva Breuninger
+    Matrikel: 266825
+    Datum: 14.06.2021
+    Quellen: Larissa Gaede, Maximilian Buckel
+    */
+
     let timeScale: number = 0.005;
     let scene: Scene = new Scene(timeScale);
 
